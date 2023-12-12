06350813397607710488885d81a6b6dbfda47354
PMwelase <146313711+PMwelase@users.noreply.github.com>
2023-12-12 08:12:10 +0200

    Update loadshedding.py
    API settings changed

--- dailyUpdate/loadshedding.py
+++ dailyUpdate/loadshedding.py
@@ -1,7 +1,7 @@
 import requests
 
 LICENSE_KEY = os.environ.get("KEY")
-eskom_url = "https://developer.sepush.co.za/business/2.0/area?id=ethekwini3-12a-cbdeast&test=current"
+eskom_url = "https://developer.sepush.co.za/business/2.0/area?id=ethekwini3-12a-cbdeast"
 payload={}
 headers = {
     'token': LICENSE_KEY,
@@ -10,18 +10,19 @@ headers = {
 response = requests.request("GET", eskom_url , headers=headers, data=payload)
 response = response.json()
 
-# print(response)
+
+
+print(response)
 stage = int(response["events"][0]["note"][6]) -1
 affected_hours = response["schedule"]["days"][0]["stages"][stage]
-print(stage)
-affected_hours = []
 # print(affected_hours)
+# print(stage)
 
 def all_affected_hours():
     all_hours = []
     for n in affected_hours:
         if 8 <= int(n[0:2]) <= 16:
-            n = n.replace('30', '00')
             all_hours.append(n)
 
     return all_hours
+    
