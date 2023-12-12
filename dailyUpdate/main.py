07935c5bd5f123c882c05e36288ee6c4dbd48cdb
PMwelase <146313711+PMwelase@users.noreply.github.com>
2023-12-12 08:13:19 +0200

    Update main.py

--- dailyUpdate/main.py
+++ dailyUpdate/main.py
@@ -20,25 +20,24 @@ if __name__ == "__main__":
     from_email='pmwelase023@student.wethinkcode.co.za'
     password=os.environ.get("PASSWORD")
 
-    morning_weather = morning_weather()
-    afternoon_weather = afternoon_weather()
-    evening_weather = evening_weather()
+    morning_weather = weather.morning_weather()
+    afternoon_weather = weather.afternoon_weather()
+    evening_weather = weather.evening_weather()
 
     content = f"Here's how your day is looking:\n{morning_weather}\n{afternoon_weather}\n{evening_weather}\n\n"
-    if rain() == True:
+    if weather.rain() == True:
         content += "You might want to carry an umbrella.\n"
 
-    if len(affected_hours) > 0:
-        content += f"There'll likely be loadshedding at the Durban Campus today at {(all_affected_hours())[0][0:5]}\n\n"
-        if len(affected_hours) == 2:
-            content += f"and at {(all_affected_hours[1][0:5])}"
-        content += f"Make sure you've saved your work before then."
+    if len(loadshedding.affected_hours) > 0:
+        content += f"There'll likely be loadshedding at the Durban Campus today at {(loadshedding.all_affected_hours())[0][0:5]}\n\n"
+        if len(loadshedding.affected_hours) == 2:
+            content += f"Make sure you've saved your work before then."
 
     else:
         content += f"We are currently NOT expecting any loadshedding today."
-        
+
     content = content.encode('ascii', 'ignore').decode('ascii')
-    
+
     for index, row in df.iterrows():
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
@@ -47,7 +46,7 @@ if __name__ == "__main__":
                 from_addr=from_email,
                 to_addrs=row["email"],
                 msg=f"Subject:Daily Update\n\nHi {row['name']},\n\n{content}\n\nWarm Regards,\nPhumelela"
-                
+
         )
 
 
